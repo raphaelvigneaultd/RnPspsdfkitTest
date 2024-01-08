40d9341331b6f9f4e599abf5e1ec64f5f9ac2b3a
Raphaël Vigneault Ð. <raphael.vigneaultd@loginnove.ca>
2024-01-08 09:41:44 -0500

    bugs - theme inheritance - pdfView runtime

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
 import {Platform, Text} from 'react-native';
+import PSPDFKitView from 'react-native-pspdfkit';
 
 const DOCUMENT =
   Platform.OS === 'ios' ? 'misc.pdf' : 'file:///android_asset/misc.pdf';
 export default class App extends Component<{}> {
+
+  //@FIXME:  RCTPSPDFKitView was not found in the UI manager
+  renderKit = () => {
+    return (
+      <PSPDFKitView
+        document={DOCUMENT}
+        configuration={{
+          showThumbnailBar: 'scrollable',
+          pageTransition: 'scrollContinuous',
+          scrollDirection: 'vertical',
+        }}
+        ref="pdfView"
+        fragmentTag="PDF1"
+        style={{flex: 1}}
+      />
+    );
+  };
+
   render() {
     return (
       <>
         <Text>Hello Blobs</Text>
+        {this.renderKit()}
       </>
     );
   }
