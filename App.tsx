9b94abce7dcaa6b2d61ba5f71c7642bb0cce2051
Raphaël Vigneault Ð. <raphael.vigneaultd@loginnove.ca>
2024-01-08 09:38:45 -0500

    ....

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Platform, Text} from 'react-native';
-import PSPDFKitView from 'react-native-pspdfkit';
 
 const DOCUMENT =
   Platform.OS === 'ios' ? 'misc.pdf' : 'file:///android_asset/misc.pdf';
